521b7a09e8872d5add12d7ba42dfe8fb78584473
lucasrocha2704 <lucas.rocha@sptech.school>
2023-10-28 19:54:52 -0300

    finalização backend histórico

--- src/models/servidorModel.js
+++ src/models/servidorModel.js
@@ -11,7 +11,20 @@ function listarServidores() {
     return database.executar(instrucao);
 }
 
+function status() {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pegarLocal()");
+    var instrucao = `
+        SELECT idServidor, 
+            DATE_FORMAT(MomentoRegistro, '%d/%m/%Y %H:%i:%s') MomentoRegistro,
+            Status
+        FROM situacaoServidor 
+        GROUP BY idServidor, MomentoRegistro;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 
 module.exports = {
     listarServidores,
+    status
 }
