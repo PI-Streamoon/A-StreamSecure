245a9587cabd4df764aa08fe146c7363dd6753cf
Thigos <dev.findviewbyid@gmail.com>
2023-11-16 01:28:14 -0300

    16/11

--- public/dashboard/dashManutencao/js/modal.js
+++ public/dashboard/dashManutencao/js/modal.js
@@ -10,7 +10,7 @@ const turnModalMoonAssistant = (element)=>{
     img.src = isClosed ? "https://img.icons8.com/ios/50/ffffff/delete-sign.png" : "https://img.icons8.com/ios/50/ffffff/astronaut.png";
     img.className = isClosed ? "" : "icon";
 
-    moonAssistantModal.style.right = isClosed ? '0px' : '-100vw';
+    moonAssistantModal.style.right = isClosed ? '100px' : '-100vw';
 
     document.body.style.overflowY = isClosed ? 'hidden' : 'auto';
 
